eb52c9360e4d043e1cdf53355597f82857f7e4ef
”killysergide” <gltk07@gmail.com>
2025-02-23 19:30:39 +0300

    2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,22 +1,28 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/cars")
-    public String Cars(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, Model model) {
+    private final CarService carService;
 
-        CarService carService = new CarServiceImpl();
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, Model model) {
+
         List<Car> carList = carService.getCars(count);
 
         model.addAttribute("carList", carList);
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,21 +1,32 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CarServiceImpl implements CarService {
 
-    @Override
-    public List<Car> getCars(int count) {
-        List<Car> carList = new ArrayList<>();
+    private final List<Car> carList;
+
+    {
+        carList = new ArrayList<>();
 
         carList.add(new Car("Toyota", "Camry", 2020));
         carList.add(new Car("Ford", "Mustang", 2021));
         carList.add(new Car("BMW", "X5", 2019));
         carList.add(new Car("Honda", "Civic", 2022));
         carList.add(new Car("Tesla", "Model 3", 2023));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+
+        if (count < 0) {
+            count = 0;
+        }
 
         if (count > carList.size()) {
             count = carList.size();
